b35f53dce6780b9ffa50bf6d3325d7240dec3892
zhongyuchen <izhongyuchen@gmail.com>
2019-04-07 11:50:08 +0800

    add

---
+++ install.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# zsh
+sudo apt-get install zsh
+sudo chsh -s /bin/zsh
+
+# oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+mv .zshrc ~/.zshrc
+
+# tmux
+sudo apt-get install tmux
+
+mv .tmux.conf ~/.tmux.conf
+
+mkdir ~/Tools
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/Tools
+tmux source-file ~/.tmux.conf
+
+# vim
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh

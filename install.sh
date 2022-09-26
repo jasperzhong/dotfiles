e963601a661886d20ccd8e7b4d8f08e9081ee472
Jasper <izhongyuchen@gmail.com>
2022-09-26 12:34:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git config --global user.name "jasperzhong"
 git config --global user.email "izhongyuchen@gmail.com"
 
 # zsh
-apt-get install -y zsh
+sudo apt-get install -y zsh
 chsh -s $(which zsh) $(whoami)
 
 # oh-my-zsh
@@ -24,14 +24,14 @@ source ~/.zshrc
 echo `which python`
 
 # tmux
-apt remove -y tmux
+sudo apt remove -y tmux
 mkdir ~/repos && cd ~/repos
 git clone https://github.com/tmux/tmux.git && cd tmux
-apt-get install -y libevent-dev libncurses5-dev automake
+sudo apt-get install -y libevent-dev libncurses5-dev automake
 git checkout 2.9a
 sh autogen.sh
 ./configure && make
-make install
+sudo make install
 
 # tmux-config
 cd ~/repos
@@ -39,19 +39,19 @@ git clone https://github.com/yuchenzhong/tmux-config.git && cd tmux-config
 ./install.sh
 
 # neovim
-add-apt-repository -y ppa:neovim-ppa/stable
-apt-get update
-apt-get install -y neovim
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim
 pip install neovim
 git clone https://github.com/yuchenzhong/nvim.git ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # nodejs
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # LSP
-apt-get install clangd-12
+sudo apt-get install clangd-12
 ln -s /usr/bin/clangd-12 /usr/bin/clangd
 pip install cmake-language-server
 pip install pyright
@@ -62,10 +62,10 @@ git clone https://github.com/universal-ctags/ctags.git && cd ctags
 ./autogen.sh
 ./configure --prefix=/usr/local
 make
-make install
+sudo make install
 
 # vim-autoformat
-apt-get install -y clang-format
+sudo apt-get install -y clang-format
 pip install autopep8
 
 
